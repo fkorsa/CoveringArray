10f41a79fff26e9f27b23596673d53e3f91d164f
Bojo <fkorsa31@gmail.com>
2014-02-19 17:28:21 -0500

    tabou avec aleatoire pour les ex aequo

--- tabou/tp2.cpp
+++ tabou/tp2.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <math.h>
 #include <chrono>
+#include <list>
 
 using namespace std;
 
@@ -62,6 +63,7 @@ CA_Solution* tabou(CA_Solution* configInit, int nombreEssais, ofstream *fichier,
     CA_Solution* meilleureConfig = new CA_Solution(*configInit); // Meilleures des configurations testées jusqu'alors
     double delta, deltaMin;
     bool minDefini, premiereIteration;
+    list<Mouvement> listeMeilleurs;
 
     // Initialisation de la liste taboue : grande matrice contenant le numero de l'iteration jusqu'a
     // laquelle l'attribut est tabou
@@ -92,6 +94,7 @@ CA_Solution* tabou(CA_Solution* configInit, int nombreEssais, ofstream *fichier,
         premiereIteration = true;
         configTestee->reinitialiserMouvementCourant();
         mouvementActuel = configTestee->mouvementCourant();
+        listeMeilleurs.clear();
         while(!mouvementActuel.estFinal)
         {
             // La methode mouvement() parcourt tous les mouvements possibles : a chaque appel,
@@ -107,12 +110,31 @@ CA_Solution* tabou(CA_Solution* configInit, int nombreEssais, ofstream *fichier,
             if((listeTaboue[mouvementActuel.mLigne][mouvementActuel.mCol][mouvementActuel.mSymbole] < totalIt
                 // Critere d'aspiration
                 || coutTest < coutMeilleure)
-                && (!minDefini || delta < deltaMin))
+                && (!minDefini || delta <= deltaMin))
             {
+                if(!minDefini || delta < deltaMin)
+                {
+                    listeMeilleurs.clear();
+                }
                 deltaMin = delta;
                 coutMin = coutTest;
                 meilleurMouvement = mouvementActuel;
                 minDefini = true;
+                listeMeilleurs.push_back(meilleurMouvement);
+            }
+        }
+        double tailleListe = listeMeilleurs.size();
+        if(tailleListe > 1)
+        {
+            double r = (double)rand()/RAND_MAX;
+            double i = 1;
+            for(list<Mouvement>::iterator it = listeMeilleurs.begin(); it != listeMeilleurs.end(); it++, i++)
+            {
+                if(r < i/tailleListe)
+                {
+                    meilleurMouvement = *it;
+                    break;
+                }
             }
         }
 
@@ -127,6 +149,7 @@ CA_Solution* tabou(CA_Solution* configInit, int nombreEssais, ofstream *fichier,
                 meilleureConfig = new CA_Solution(*configTestee);
                 coutMeilleure = coutMin;
                 itSansMvtCpt = 0;
+                //cout << "Meilleure solution trouvee a l'iteration : " << totalIt << " de cout : " << coutMeilleure << endl;
             }
             vraisMouvementsTotal++;
         }
@@ -137,18 +160,136 @@ CA_Solution* tabou(CA_Solution* configInit, int nombreEssais, ofstream *fichier,
         if(fichier && totalIt%50 == 0)
         {
             fichierLocal << totalIt << " " << coutActuelle << endl;
+            cout << totalIt << " " << coutActuelle << endl;
         }
+//        if(totalIt%100 == 0)
+//        {
+//            cout << totalIt << " " << coutActuelle << endl;
+//        }
 	}
     meilleureConfig->nbIt = totalIt;
     meilleureConfig->nbMvt = vraisMouvementsTotal;
 	return(meilleureConfig);
 }
 
+void genererResultats(int nbExec)
+{
+    ifstream infile("inputData");
+    ofstream outfile("output");
+    int nbSymboles, nbColonnes, nbLignes, longueurListe;
+    double coutMoyen, itMoyen, mvMoyen, tempsMoyen;
+    double coutMin, itMin, mvMin, tempsMin;
+    double coutMax, itMax, mvMax, tempsMax, temps;
+    chrono::time_point<chrono::system_clock> start;
+    chrono::duration<double> realTime;
+    CA_Solution* solution;
+    if(outfile.is_open())
+    {
+        outfile << "NombreSymboles NombreColonnes NombreLignes CoutMin CoutMoyen CoutMax ItMin ItMoyen ItMax MvMin MvMoyen MvMax TempsMin TempsMoyen TempsMax" << endl;
+        while (infile >> nbSymboles >> nbColonnes >> nbLignes >> longueurListe)
+        {
+            coutMoyen = 0;
+            itMoyen = 0;
+            mvMoyen = 0;
+            tempsMoyen = 0;
+            coutMin = 0;
+            itMin = 0;
+            mvMin = 0;
+            tempsMin = 0;
+            coutMax = 0;
+            itMax = 0;
+            mvMax = 0;
+            tempsMax = 0;
+            cout << nbSymboles << " "
+                    << nbColonnes << " "
+                    << nbLignes << " " << endl;
+            for(int i = 0; i < nbExec; i++)
+            {
+                CA_Solution* configInit = configurationAleatoire(nbSymboles, nbColonnes, nbLignes);
+                start = chrono::system_clock::now();
+                solution = tabou(configInit, 10000, NULL, longueurListe);
+                realTime = chrono::system_clock::now()-start;
+                temps = 1000*realTime.count();
+                if(i == 0)
+                {
+                    coutMin = solution->erreurs;
+                    itMin = solution->nbIt;
+                    mvMin = solution->nbMvt;
+                    tempsMin = temps;
+                }
+                if(coutMin > solution->erreurs)
+                {
+                    coutMin = solution->erreurs;
+                }
+                if(itMin > solution->nbIt)
+                {
+                    itMin = solution->nbIt;
+                }
+                if(mvMin > solution->nbMvt)
+                {
+                    mvMin = solution->nbMvt;
+                }
+                if(tempsMin > temps)
+                {
+                    tempsMin = temps;
+                }
+                if(coutMax < solution->erreurs)
+                {
+                    coutMax = solution->erreurs;
+                }
+                if(itMax < solution->nbIt)
+                {
+                    itMax = solution->nbIt;
+                }
+                if(mvMax < solution->nbMvt)
+                {
+                    mvMax = solution->nbMvt;
+                }
+                if(tempsMax < temps)
+                {
+                    tempsMax = temps;
+                }
+                coutMoyen += solution->erreurs;
+                itMoyen += solution->nbIt;
+                mvMoyen += solution->nbMvt;
+                tempsMoyen += temps;
+                delete solution;
+            }
+            coutMoyen /= nbExec;
+            itMoyen /= nbExec;
+            mvMoyen /= nbExec;
+            tempsMoyen /= nbExec;
+            tempsMoyen *= (8.6/5.4);
+            tempsMin *= (8.6/5.4);
+            tempsMax *= (8.6/5.4);
+            outfile << nbSymboles << " "
+                    << nbColonnes << " "
+                    << nbLignes << " "
+                    << coutMin << " "
+                    << coutMoyen << " "
+                    << coutMax << " "
+                    << itMin << " "
+                    << itMoyen << " "
+                    << itMax << " "
+                    << mvMin << " "
+                    << mvMoyen << " "
+                    << mvMax << " "
+                    << tempsMin << " "
+                    << tempsMoyen << " "
+                    << tempsMax << endl;
+        }
+        outfile.close();
+    }
+}
+
 int main()
 {
     int seed = time(NULL);
-    //srand(seed);
-    CA_Solution* configInit = configurationAleatoire(3, 60, 21);
-    CA_Solution* configTabou = tabou(configInit, 50000, NULL, 40);
+    srand(seed);
+
+    CA_Solution* configInit = configurationAleatoire(3, 20, 16);
+    CA_Solution* configTabou = tabou(configInit, 1000, NULL, 30);
     cout << "Erreurs : " << configTabou->erreurs << " iterations : " << configTabou->nbIt << endl;
+
+    //genererResultats(1);
 }
